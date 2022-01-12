29c6f20939def5f12b729fbd630d27b4aee60891
lasvistass <spinelli1k90@gmail.com>
2022-01-12 15:40:11 +0100

    aggiunta e testing restTemplate Login

---
+++ src/main/java/com/netgroup/exceldemo/clientScheduleLogin/model/RegistrationUser.java
@@ -0,0 +1,16 @@
+package com.netgroup.exceldemo.clientScheduleLogin.model;
+
+
+public class RegistrationUser extends User {
+
+	private String role;
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+}
---
+++ src/main/java/com/netgroup/exceldemo/clientScheduleLogin/model/ResponseToken.java
@@ -0,0 +1,15 @@
+package com.netgroup.exceldemo.clientScheduleLogin.model;
+
+
+public class ResponseToken {
+
+	private String token;
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+}
